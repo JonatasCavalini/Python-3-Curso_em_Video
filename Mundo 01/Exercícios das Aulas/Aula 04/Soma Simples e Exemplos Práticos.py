b5ea9ad1293a1c311e5c42c52b5415c181fccdac
Jônatas Cavalini <jonatas.cavalini@gmail.com>
2022-03-16 11:41:39 -0300

    Initial Commit

---
+++ Mundo 01/Exercícios das Aulas/Aula 04/Soma Simples e Exemplos Práticos.py
@@ -0,0 +1,9 @@
+print(7 + 4)  # 7 + 4 = 11
+
+print('7' + '4')  # '7' + '4' = '74'
+
+print('7', '4')  # '7' , '4' = '7 4'
+
+""" NOTA: Concatenação pode ser feita através de virgula ou o sinal de '+'."""
+
+print('Olá', 5)
